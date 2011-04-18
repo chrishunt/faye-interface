d7469f04976e8b4e53bb4839a17b2fad4a1b4544
Chris Hunt <huntca@gmail.com>
2011-04-18 09:35:36 -0700

    Changed class name to FayeInterface

---
+++ src/net/progeny/uswdss/chci/faye/FayeInterface.java
@@ -0,0 +1,12 @@
+package net.progeny.uswdss.chci.faye;
+
+public class FayeInterface {
+  public static void main(String args[]){
+    FayeInterface faye = new FayeInterface();
+    System.out.println("Hello World");
+    System.exit(0);
+  }
+
+  public FayeInterface () {
+  }
+}

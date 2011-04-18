7c79fc7d99670e5ce105e79db2af96ba21c0738b
Chris Hunt <huntca@gmail.com>
2011-04-18 09:30:27 -0700

    Added build file and hello world

---
+++ src/net/progeny/test/HttpTest.java
@@ -0,0 +1,12 @@
+package net.progeny.test;
+
+public class HttpTest {
+  public static void main(String args[]){
+    HttpTest test = new HttpTest();
+    System.out.println("Hello World");
+    System.exit(0);
+  }
+
+  public HttpTest() {
+  }
+}
